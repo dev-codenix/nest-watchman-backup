2eb7075a0246f18245da45d23a8fa2cbf2493af2
MDaneshjoo <m_daneshjoo@felixin.io>
2023-04-14 01:34:03 +0330

    feat(discord): add mention list in constructor in order to mention people

--- src/strategies/discord.strategy.ts
+++ src/strategies/discord.strategy.ts
@@ -12,14 +12,30 @@ export class DiscordBaseStrategy extends BaseStrategy {
   @Inject()
   embedBuilder: EmbedBuilder;
 
-  constructor(private webHookUrl: string) {
+  private mentioned: Array<string>;
+  /**
+   * @param webHookUrl discord webhook url
+   * @param mentionList if you want mention some use this method. note that don't use @
+   * */
+  constructor(
+    private webHookUrl: string,
+    mentionList?: Array<'here' | 'everyone' | string>,
+  ) {
     super();
+    if (mentionList) this.mention(mentionList);
   }
 
   protected send(discordBody: DiscordBodyInterface): Subscription {
     return this.httpService.post(this.webHookUrl, discordBody).subscribe();
   }
 
+  private mention(mentionList: Array<'here' | 'everyone' | string>): void {
+    this.mentioned = mentionList.map((person) =>
+      person === 'here' || person === 'everyone'
+        ? `@${person}`
+        : `@<${person}>`,
+    );
+  }
   watchMessageFormat(): DiscordBodyInterface {
     /**
      * @see {@link https://discordjs.guide/popular-topics/embeds.html#embed-preview}
@@ -51,8 +67,12 @@ export class DiscordBaseStrategy extends BaseStrategy {
       });
     if (this.exception.uuid)
       embed.addFields({ name: 'Tracking Id', value: this.exception.uuid });
-    return {
+    const discordBody: DiscordBodyInterface = {
       embeds: [embed],
     };
+    if (this.mentioned.length)
+      discordBody.content = `${this.mentioned.join(', ')}`;
+
+    return discordBody;
   }
 }
