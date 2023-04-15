34b9f6e73790127afde91c4e6143d18d09e007a8
MDaneshjoo <m_daneshjoo@felixin.io>
2023-04-15 20:21:55 +0330

    add todo

--- src/Watchman.module.ts
+++ src/Watchman.module.ts
@@ -14,6 +14,7 @@ import {
   BaseStrategy,
 } from './strategies';
 // TODO able to inject custom strategy in
+// TODO test useClass injection
 @Module({})
 export class WatchmanModule {
   //TODO: fix forRoot method in config.strategy
